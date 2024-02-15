b9beae7b7b9dae44544f32d2ef202b5886f9cc57
ThibaultGM78 <thibault.garciam@gmail.com>
2024-02-15 20:40:32 +0100

    ajout methode getProduct backend

--- backend/database.sql
+++ backend/database.sql
@@ -5,6 +5,7 @@ USE bookish;
 CREATE TABLE bookish_product (
     id_product INT AUTO_INCREMENT,
     product_name VARCHAR(30),
+    product_author VARCHAR(30),
     product_price DECIMAL(6, 2),
     product_category VARCHAR(15),
     product_stock INT,
@@ -13,22 +14,22 @@ CREATE TABLE bookish_product (
     PRIMARY KEY(id_product)
 );
 
-INSERT INTO bookish_product (product_name, product_price, product_category, product_stock, product_desc, product_img) VALUES
-    ('Livre A', 19.99, 'fantastic', 50, 'Description du Livre A', 'img/livreA.jpg'),
-    ('Livre B', 29.99, 'fantastic', 30, 'Description du Livre B', 'img/livreB.jpg'),
-    ('Livre C', 39.99, 'fantastic', 20, 'Description du Livre C', 'img/livreC.jpg'),
-    ('Livre D', 49.99, 'fantastic', 25, 'Description du Livre D', 'img/livreD.jpg'),
-    ('Livre E', 59.99, 'romance', 40, 'Description du Livre E', 'img/livreE.jpg'),
-    ('Livre F', 69.99, 'romance', 15, 'Description du Livre F', 'img/livreF.jpg'),
-    ('Livre G', 79.99, 'romance', 35, 'Description du Livre G', 'img/livreG.jpg'),
-    ('Livre H', 89.99, 'romance', 10, 'Description du Livre H', 'img/livreH.jpg'),
-    ('Livre I', 99.99, 'adventure', 45, 'Description du Livre I', 'img/livreI.jpg'),
-    ('Livre J', 109.99, 'adventure', 22, 'Description du Livre J', 'img/livreJ.jpg'),
-    ('Livre K', 119.99, 'adventure', 18, 'Description du Livre K', 'img/livreK.jpg'),
-    ('Livre L', 129.99, 'adventure', 27, 'Description du Livre L', 'img/livreL.jpg'),
-    ('Livre M', 139.99, 'fart', 33, 'Description du Livre M', 'img/livreM.jpg'),
-    ('Livre N', 149.99, 'fart', 12, 'Description du Livre N', 'img/livreN.jpg'),
-    ('Livre O', 159.99, 'fart', 8, 'Description du Livre O', 'img/livreO.jpg');
+INSERT INTO bookish_product (product_name, product_author, product_price, product_category, product_stock, product_desc, product_img) VALUES
+    ('Livre A', 'François', 19.99, 'fantastic', 50, 'Description du Livre A', 'img/livreA.jpg'),
+    ('Livre B', 'François',29.99, 'fantastic', 30, 'Description du Livre B', 'img/livreB.jpg'),
+    ('Livre C', 'François',39.99, 'fantastic', 20, 'Description du Livre C', 'img/livreC.jpg'),
+    ('Livre D', 'François', 49.99, 'fantastic', 25, 'Description du Livre D', 'img/livreD.jpg'),
+    ('Livre E', 'François', 59.99, 'romance', 40, 'Description du Livre E', 'img/livreE.jpg'),
+    ('Livre F', 'Michel', 69.99, 'romance', 15, 'Description du Livre F', 'img/livreF.jpg'),
+    ('Livre G', 'Michel', 79.99, 'romance', 35, 'Description du Livre G', 'img/livreG.jpg'),
+    ('Livre H', 'Michel', 89.99, 'romance', 10, 'Description du Livre H', 'img/livreH.jpg'),
+    ('Livre I', 'Michel', 99.99, 'adventure', 45, 'Description du Livre I', 'img/livreI.jpg'),
+    ('Livre J', 'Michel', 109.99, 'adventure', 22, 'Description du Livre J', 'img/livreJ.jpg'),
+    ('Livre K', 'Sylvie', 119.99, 'adventure', 18, 'Description du Livre K', 'img/livreK.jpg'),
+    ('Livre L', 'Sylvie', 129.99, 'adventure', 27, 'Description du Livre L', 'img/livreL.jpg'),
+    ('Livre M', 'Sylvie', 139.99, 'fart', 33, 'Description du Livre M', 'img/livreM.jpg'),
+    ('Livre N', 'Sylvie', 149.99, 'fart', 12, 'Description du Livre N', 'img/livreN.jpg'),
+    ('Livre O', 'Sylvie', 159.99, 'fart', 8, 'Description du Livre O', 'img/livreO.jpg');
 
 
     
--- backend/server.js
+++ backend/server.js
@@ -53,6 +53,71 @@ app.get('/productSearch/:search', (req, res) => {
     });
 });
 
+//Exemple: /getProduct?vTrie=value&auteur=value&category=value&search=value
+app.get('/getProduct', (req, res) => {
+
+    var { title ,author, category, search,vTrie } = req.query;
+    
+    var partTarget = '';
+    var partSearch = '';
+
+    function addToPartTarget(column,value,partTarget){
+
+        if(partTarget != ''){
+            partTarget += 'AND '
+        }
+        partTarget += column + " = '" + value + "' ";
+
+        return partTarget
+    }
+    function addToPartSearch(column,value,partSearch){
+        if(partSearch != ''){
+            partSearch += 'OR '
+        }
+        partSearch += column + " LIKE '%" + value + "%' ";
+
+        return partSearch
+    }
+
+    if(title){
+        partTarget = addToPartTarget('product_name',title,partTarget);
+    }
+    else{
+        partSearch = addToPartSearch('product_name',search,partSearch)
+    }
+
+    if(author){
+        partTarget = addToPartTarget('product_author',author,partTarget);
+    }
+    else{
+        partSearch = addToPartSearch('product_author',search,partSearch)
+    }
+
+    if(category){
+        partTarget = addToPartTarget('product_category',category,partTarget);
+    }
+    else{
+        partSearch = addToPartSearch('product_category',search,partSearch)
+    }
+
+    var sql = "SELECT * FROM bookish_product WHERE " + partTarget;
+    if(partSearch != ''){
+        sql += "AND (" + partSearch + ")"
+    }
+
+    console.log(sql);
+    db.query(sql, [title,author,category], (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        if (data.length === 0) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        return res.json(data);
+    });
+   
+  });
+
 app.listen(8081, () => {
     console.log("listening");
 })
